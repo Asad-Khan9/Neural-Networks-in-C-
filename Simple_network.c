416679810446be49864d32d3cedfe78ce435bf61
Asad-Khan9 <78179407+Asad-Khan9@users.noreply.github.com>
2023-11-18 21:27:29 +0300

    Update Simple_network.c

--- Simple_network.c
+++ Simple_network.c
@@ -11,7 +11,7 @@ float train[][2] = {
 };
 #define train_count sizeof(train) / sizeof(train[0])
     // y = x*w
-float rand_float(){
+float rand_float(){ // to retrieve random values for weights and biases 
     return (float) rand()/ (float) RAND_MAX;
 }
 float cost(float w, float b){
@@ -19,7 +19,7 @@ float cost(float w, float b){
     for(size_t i = 0; i < train_count; ++i){
         float x = train[i][0];
         float y = x*w + b;
-        float d = y - train[i][1];
+        float d = y - train[i][1]; // distance between the actual value and the predicted 
         result += d*d;
         //printf("actual %f, expected %f\n", y, train[i][1]); 
     }
@@ -27,7 +27,7 @@ float cost(float w, float b){
     return result;
 }
 int main(){
-    srand(69);
+    srand(25);
     float result = 0;
     float w = rand_float()*10.0f;
     float b = rand_float()*5.0f;
@@ -39,15 +39,18 @@ int main(){
     printf("%f\n", cost(w, b));
     for (size_t i = 0; i < 10000; ++i){
         float c = cost(w, b);
-        float dw = (cost(w+eps, b) - c)/eps;
-        float db = (cost(w, b+eps) - c)/eps;
-        w-= rate*dw;
-        b -= rate*db;
-        if(i%50 == 0){
+        // gradient descent 
+        float dw = (cost(w+eps, b) - c)/eps; //with respect to weights
+        float db = (cost(w, b+eps) - c)/eps; // with respect to bias 
+        w-= rate*dw;  // updating the weights after descent 
+        b -= rate*db; // updating the biases after descent 
+
+        
+        if(i%50 == 0){  // printing the outputs at every 50th iteration
             printf("\ncost = %f, w = %f, b = %f \n", cost(w, b), w, b); 
         }
     }
-
+    // final result 
     printf("-------------------------------");
     printf("\n%f, %f", w, b);
     return 0;
